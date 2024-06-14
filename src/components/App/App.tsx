f1f6ce9f5d403e75720c7ed7e7a6719a72e9c8b4
ViacheslavPiskun <vyacheslav2905@gmail.com>
2024-06-13 19:54:07 -0600

    create react vite template files

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css'
+
+const App = () => {
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>Vite + React + TS + ESLint template</h1>
+    </div>
+  )
+}
+
+export default App
