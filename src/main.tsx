7d2175478da92e45968d26d6929d56ce5c8d16b3
ViacheslavPiskun <vyacheslav2905@gmail.com>
2024-06-13 20:13:58 -0600

    add aliases for tsconfig and vite.config.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
 
-import App from "./components/App";
+import App from "~components/App";
+
+import '~styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~components': path.resolve(__dirname, './src/components'),
+      '~styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
 })
